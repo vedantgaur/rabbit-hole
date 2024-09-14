74b04b760bf0352d798c060021b56dd93d8160fa
Justin Wang <justinwang46@gmail.com>
2024-09-14 14:22:40 -0400

    add init modal endpoint

---
+++ modal_endpoint/download_llama.py
@@ -0,0 +1,56 @@
+import modal
+
+MODELS_DIR = "/llamas"
+
+DEFAULT_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+volume = modal.Volume.from_name("llamas", create_if_missing=True)
+
+image = (
+    modal.Image.debian_slim(python_version="3.10")
+    .pip_install(
+        [
+            "huggingface_hub",  # download models from the Hugging Face Hub
+            "hf-transfer",  # download models faster with Rust
+        ]
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+)
+
+
+MINUTES = 60
+HOURS = 60 * MINUTES
+
+
+app = modal.App(
+    image=image, secrets=[modal.Secret.from_name("huggingface-secret")]
+)
+
+
+@app.function(volumes={MODELS_DIR: volume}, timeout=4 * HOURS)
+def download_model(model_name, force_download=False):
+    from huggingface_hub import snapshot_download
+
+    volume.reload()
+
+    snapshot_download(
+        model_name,
+        local_dir=MODELS_DIR + "/" + model_name,
+        ignore_patterns=[
+            "*.pt",
+            "*.bin",
+            "*.pth",
+            "original/*",
+        ],  # Ensure safetensors
+        force_download=force_download,
+    )
+
+    volume.commit()
+
+
+@app.local_entrypoint()
+def main(
+    model_name: str = DEFAULT_NAME,
+    force_download: bool = False,
+):
+    download_model.remote(model_name, force_download)
